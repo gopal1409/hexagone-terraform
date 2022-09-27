0b93d0875fc4fe806adb0bfe04b1e01de94d986f
gopal <gopal1409@gmail.com>
2022-09-27 09:23:59 +0000

    terraform

---
+++ c1-version.tf
@@ -0,0 +1,14 @@
+####we will create a provider block
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {}
+}
+####create a resource group
---
+++ c2.resourcegrp.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "ag_rg" {
+  name     = "example-resources-gopal"
+  location = "West Europe"
+}
