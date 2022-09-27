22bb2195741b00d034fe8cdcef59d495697a4e1a
gopal <gopal1409@gmail.com>
2022-09-27 10:34:44 +0000

    teraform with storage

--- c3-storageaccount.tf
+++ c3-storageaccount.tf
@@ -1,21 +1,12 @@
-resourceresource "azurerm_storage_account" "appsstore" {
+resource "azurerm_storage_account" "appsstore" {
   name                     = "appstore12345"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-
-  tags = {
-    environment = "staging"
-  }
-} "azurerm_storage_account" "example" {
-  name                     = "storageaccountname"
   resource_group_name      = azurerm_resource_group.ag_rg.name
-  location                 = azurerm_resource_group.ag_rg.location
+  location                 = azurerm_resource_group.ag_rg.id
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
   tags = {
     environment = "staging"
   }
-}
+
+} 
