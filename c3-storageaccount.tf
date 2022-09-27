d74f704281f66b3aa4f998f26abf355dfcc38171
gopal <gopal1409@gmail.com>
2022-09-27 10:37:14 +0000

    teraform with storage

--- c3-storageaccount.tf
+++ c3-storageaccount.tf
@@ -1,7 +1,7 @@
 resource "azurerm_storage_account" "appsstore" {
   name                     = "appstore12345"
   resource_group_name      = azurerm_resource_group.ag_rg.name
-  location                 = azurerm_resource_group.ag_rg.id
+  location                 = azurerm_resource_group.ag_rg.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
